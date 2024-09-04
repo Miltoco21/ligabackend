93e210a6031d98481c1fcfc2ae076b422c1a4d92
MiltonP <milton206@gmail.com>
2024-09-03 21:27:17 -0400

    jugadores ruta

--- db/database.sql
+++ db/database.sql
@@ -1,3 +1,4 @@
+-- Active: 1724694582988@@ben5ld9pxvoeo3lkstud-mysql.services.clever-cloud.com@3306@ben5ld9pxvoeo3lkstud
  CREATE DATABASE IF NOT EXISTS companydb;
  use company;
  CREATE TABLE empleados (
@@ -47,3 +48,80 @@ CREATE TABLE usuarios (
    PRIMARY KEY(id)
  );
 
+ CREATE TABLE equipos (
+   id INT(11)NOT NULL AUTO_INCREMENT,
+   nombre VARCHAR(255)DEFAULT NULL,
+   logo VARCHAR(255)DEFAULT NULL,
+   email VARCHAR(255)DEFAULT NULL,
+   representante VARCHAR(255)DEFAULT NULL,
+   capitan VARCHAR(255)DEFAULT NULL,
+   
+   PRIMARY KEY(id)
+ );
+use ben5ld9pxvoeo3lkstud
+
+create table jugadores (
+  id bigint primary key ,
+  nombre VARCHAR(255)DEFAULT NULL,
+  posicion VARCHAR(255)DEFAULT NULL,
+  equipo_id bigint references equipos (id),
+  edad int not null
+  
+);
+
+create table partidos(
+  id bigint primary key ,
+  equipoLocal_id bigint references equipos  (id),
+  equipoVisita_id bigint references equipos  (id),
+  fechaPartido date not null,
+  resultadoLocal VARCHAR(255)DEFAULT NULL,
+  resultadoVisita VARCHAR(255)DEFAULT NULL
+);
+
+
+create table estadisticas (
+  id bigint primary key ,
+  equipo_id  bigint references equipos (id),
+  partidosJugados int default 0,
+  ganados int default 0,
+  perdidos int default 0,
+  empates int default 0,
+  puntos int default 0
+);
+create table tarjetas (
+  id bigint primary key ,
+  partido_id bigint references partidos (id),
+  jugador_id bigint references jugadores (id),
+  tarjeta VARCHAR(255)DEFAULT NULL
+ 
+);
+
+create table goleadores (
+  id bigint primary key ,
+ partido_id bigint references partidos (id),
+  jugador_id bigint references jugadores (id)
+  
+);
+create table pasadores (
+  id bigint primary key,
+  partido_id bigint references partidos(id),
+  jugador_id bigint references jugadores(id),
+  goleador_id bigint references goleadores(id)
+);
+alter TABLE jugadores 
+  DROP COLUMN edad,
+  ADD fechaNacimiento VARCHAR(255)DEFAULT NULL
+
+alter TABLE jugadores 
+ADD email VARCHAR(255)DEFAULT NULL
+
+ 
+alter TABLE jugadores 
+ADD rut VARCHAR(255)DEFAULT NULL
+
+ALTER TABLE jugadores 
+MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+ALTER TABLE jugadores DROP PRIMARY KEY;
+
+ALTER TABLE jugadores MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
