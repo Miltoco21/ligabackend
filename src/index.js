d0fc90f5643977e47c05c5e371b0f2c491dbe76b
MiltonP <milton206@gmail.com>
2025-01-02 19:01:23 -0300

    fix app

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
-// import app from "./app.js";
-// import { PORT } from "./config.js";
-// import { pool } from "./db.js";
-
-// const startServer = async () => {
-//   try {
-//     await pool.query("SELECT 1"); // Test database connection
-//     console.log("DB connection ok ");
-
-//     app.listen(PORT, () => {
-//       console.log(`Escuchando puerto`, PORT);
-//     });
-//   } catch (error) {
-//     console.error("Error al coneactar a database: ", error);
-//   }
-// };
-
-// startServer();
-// index.js
 import app from "./app.js";
 import { PORT } from "./config.js";
 import { pool } from "./db.js";
@@ -34,4 +15,8 @@ const startServer = async () => {
   }
 };
 
-startServer();
+if (process.env.NODE_ENV !== "production") {
+  startServer();
+}
+
+export default app;
