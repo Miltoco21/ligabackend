8f27d7e69e3e1fa717dc6a820951b20bab867c6d
MiltonP <milton206@gmail.com>
2025-01-02 19:04:38 -0300

    rutas fix vercel

--- src/app.js
+++ src/app.js
@@ -19,14 +19,22 @@ app.use(express.json())//json antesde las rutas
 
 
 
-app.use('/',rutaEmpleados)
-app.use('/',rutaHome)
-app.use('/',rutaIndex)
-app.use('/',rutaRegistro)
-app.use('/',rutaLogin)
-app.use('/',rutaUsuarios)
-app.use('/',rutaEquipos)
-app.use('/',rutaJugadores)
+// app.use('/',rutaEmpleados)
+// app.use('/',rutaHome)
+// app.use('/',rutaIndex)
+// app.use('/',rutaRegistro)
+// app.use('/',rutaLogin)
+// app.use('/',rutaUsuarios)
+// app.use('/',rutaEquipos)
+// app.use('/',rutaJugadores)
+
+app.use('/empleados', rutaEmpleados);
+app.use('/home', rutaHome);
+app.use('/registro', rutaRegistro);
+app.use('/login', rutaLogin);
+app.use('/usuarios', rutaUsuarios);
+app.use('/equipos', rutaEquipos);
+app.use('/jugadores', rutaJugadores);
 
 
 
