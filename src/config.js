3b7621b016545cbc7185249698619053ace21948
MiltonP <milton206@gmail.com>
2025-01-02 17:45:47 -0300

    ambio configc

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 
 config()
 
-export const PORT = process.env.PORT|| 4000
+export const PORT = process.env.PORT|| 8000
 
 export const DB_USER = process.env.DB_USER|| 'root'
 
